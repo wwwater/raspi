218eb0dcada9d0252f5c13266a62a5e16cf31c59
Raspi <daria.shubina@gmail.com>
2015-09-10 21:57:31 +0200

    Script for starting music after raspi (re)boot

---
+++ start_music.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+killall mopidy
+mopidy &
+sleep 30s
+echo "@MAIN:PWR=On" | telnet 192.168.1.25 50000 &>/dev/null
+mpc load meow;
+#mpc ls | mpc add; 
+mpc random; mpc repeat; 
+mpc volume 50
+mpc play
+~/ping_me.sh &
+cd server; python server.py > /dev/null 2>&1 &
+cd ~
